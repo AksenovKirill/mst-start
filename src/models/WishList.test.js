3433f106c0284c59f05e3a8c191e2d7bf833e52e
Kirill_Aksenov <aksenovkirill@yandex.ru>
2022-09-23 20:15:05 +0300

    lesson 4

--- src/models/WishList.test.js
+++ src/models/WishList.test.js
@@ -1,4 +1,5 @@
 import { getSnapshot, onSnapshot, onPatch } from "mobx-state-tree";
+import { reaction } from "mobx";
 import { WishList, WishListItem } from "./WishList";
 
 it("can create a instance of a model", () => {
@@ -65,17 +66,34 @@ it("can add new items", () => {
 
   expect(states).toMatchSnapshot();
 });
-it("can add new items", () => {
-  const list = WishList.create();
-  list.add(
-    WishListItem.create({
-      name: "Chesterton",
-      price: 10,
-    })
+
+it("can calculate the total price of a wishlist", () => {
+  const list = WishList.create({
+    items: [
+      {
+        name: "Machine Gun Preacher",
+        price: 7.35,
+        image: "https://images-na.ssl-images-amazon.com/images/I/91AFFK9fwkL._SY445_.jpg",
+      },
+      {
+        name: "LEGO Mindstorms EV3",
+        price: 349.95,
+        image: "https://images-na.ssl-images-amazon.com/images/I/71CpQw%2BufNL._SL1000_.jpg",
+      },
+    ],
+  });
+
+  expect(list.totalPrice).toBe(357.3);
+
+  let changed = 0;
+  reaction(
+    () => list.totalPrice,
+    () => changed++
   );
 
-  expect(list.items.length).toBe(1);
-  expect(list.items[0].name).toBe("Chesterton");
-  list.items[0].changeName("Book of G.K. Chesterton");
-  expect(list.items[0].name).toBe("Book of G.K. Chesterton");
+  expect(changed).toBe(0);
+  console.log(list.totalPrice);
+  list.items[0].changeName("Test");
+  expect(changed).toBe(0);
+  list.items[0].changePrice(10);
 });
